f78229b11a81015e2f32609bda77a83dcec753ad
hduog0199 <haiduongk35@gmail.com>
2022-06-15 21:35:19 +0700

    Them Dang ki va xem lich lam viec

---
+++ BUS_NHOM7_DOANCK/BUS_XemLichLamViec.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using DB;
+namespace BUS
+{
+    public class BUS_XemLichLamViec
+    {
+        public static DataTable XemLichLamViec(int StaffId)
+        {
+            DataTable table = new DataTable();
+            table = DB_NV_XemLichLamViec.LoadLichLamViec(StaffId);
+            return table;
+        }
+    }
+}
---
+++ DB_NHOM7_DOANCK/DB_NV_DangKiLichRanh.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+namespace DB
+{
+    public class DB_NV_DangKiLichRanh
+    {
+        public static int InsertDKLR(int StaffID, DateTime StartTime, DateTime EndTime)
+        {
+            SqlConnection connection;
+            String str= @"Data Source=DESKTOP-RQOM2TC\DUONG;Initial Catalog=Vaccination;Integrated Security=True";
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            connection = new SqlConnection(str);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("insert into Schedules(StaffId, StartTime, EndTime) values (@a,@b,@c)", connection);
+            cmd.Parameters.AddWithValue("@a", StaffID);
+            cmd.Parameters.AddWithValue("@b", StartTime);
+            cmd.Parameters.AddWithValue("@c", EndTime);
+            int rs = cmd.ExecuteNonQuery();
+            connection.Close();
+            return rs;
+        }
+        public static DataTable LoadLichDangKi()
+        {
+            SqlConnection connection;
+            String str = @"Data Source=DESKTOP-RQOM2TC\DUONG;Initial Catalog=Vaccination;Integrated Security=True";
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+            connection = new SqlConnection(str);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT TOP 7 StartTime,EndTime FROM Schedules", connection);    //cau truy van
+            cmd.ExecuteNonQuery();
+            connection.Close();  
+            adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(table);
+            return table;
+        }
+    }
+}
